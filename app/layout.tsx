0ebca973c3d3c97097c000115d31f1efa39c1ba9
seantrgyt <94722625+seantrgyt@users.noreply.github.com>
2025-06-07 22:27:19 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+// app/layout.tsx
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '@SeanTRG | guns.lol',
+  description: 'your local memer',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
