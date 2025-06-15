bcc7b54c63c4f41a11b0199f7d122d5e982d60a3
seantrgyt <94722625+seantrgyt@users.noreply.github.com>
2025-06-15 19:26:55 -0400

    Delete app/middleware.ts

--- app/middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/discord') {
-    return NextResponse.redirect('https://youtube.com/@seantrg')
-  }
-}
